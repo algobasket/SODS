2cd3645e24f0da8e2cb5dd71f3957be62a5a767f
algobasket <algobasket@gmail.com>
2024-02-27 15:16:28 +0530

    test

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+echo "Switching to branch master"
+git checkout master
+
+
+echo "Building app ..."
+npm run build
+
+echo "Deploying files to server ....."
+scp -r build/* algobasket@128.199.71.49:/var/www/128.199.71.49/
+
+echo "Done!"
